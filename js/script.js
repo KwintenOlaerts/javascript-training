1f38c8985d038bf93312b6d3cb54f674b21dfa04
Kwinten Olaerts <kwintenolaerts@hotmail.com>
2019-01-15 16:49:14 +0100

    very basic javascript training

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let test = 0;
+
+for (i = 0; i <= 90; i++) {
+    test = test + 1;
+    console.log(i);
+  };
+
+
---
+++ js/script2.js
@@ -0,0 +1,20 @@
+var rank = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(rank);
+
+let som = 0
+
+for (let i = 0; i < rank.length; i++){
+    // element i uit de array optellen bij som
+
+    console.log("Getal uit array:", rank[i])
+
+    som += rank[i]
+
+    console.log("De som is nu:", som)
+}
+
+console.log("het totaal is:", som)
+
+
+var actors = ['Stalone','Mercury','Lerr']
+console.log(actors);
